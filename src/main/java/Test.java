16fcdf03b9e5ba4d085ecc9546c46fe8e737e1a6
Vishal Jumnani <vishalj64@users.noreply.github.com>
2022-10-01 21:06:14 +0530

    git-cheat-sheet

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,12 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+        // to add the changes/ file to commit -> git add
+        // to commit -> git commit -m 'commit-msg'
+        //to push -> git push
+        // to fetch changes of current branch -> git pull
+        // to fetch changes of main into current branch -> git pull origin main
+
     }
 }
